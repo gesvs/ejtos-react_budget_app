dfdd224e5c4f58c80bfe4fbf46a14a0b38d84815
gesvs <jesusdiazalcaide@gmail.com>
2023-08-16 12:40:53 -0400

    correcto total

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-	const { budget, dispatch,expenses, currency } = useContext(AppContext);
+	const { budget, dispatch,expenses, currency } = useContext(AppContext);  
 
 	const changeBudget = (val)=>{
 		const totalExpenses = expenses.reduce((total, item) => {
@@ -11,8 +11,7 @@ const Budget = () => {
 
 		
 		if(val<totalExpenses) {
-			alert("You cannot reduce the budget value lower than expended!");
-            // that is already allocated
+			alert("You cannot reduce the budget that is already allocated!");
 		} else {
 			dispatch({
 				type: 'SET_BUDGET',
@@ -24,13 +23,9 @@ const Budget = () => {
 	return (
 		<div className='alert alert-secondary'>
             <span>Budget: {currency}</span>
-			<input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
+            <input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
 		</div>
 	);
 };
 
 export default Budget;
-
-		
-
-
